c3420bacab57cf397e70eccb726f3b6066dded53
izaak svec <slavi.svec@gmail.com>
2021-06-30 11:03:03 +1000

    Added medium icon to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import GitHub from '../../images/GitHub64.png'
 import Twitter from '../../images/Twitter64.png'
 import Linkedin from '../../images/Linkedin64.png'
 import CodePen from '../../images/CodePen64.png'
+import Medium from '../../images/Medium-large1.png'
 import Image from '../Image/Image'
 import './styles.css'
 
@@ -14,22 +15,31 @@ const Footer = () => (
           href="https://www.linkedin.com/in/slavi-svec-262949204/"
           target="_blank"
         >
-          <Image src={Linkedin} />
+          <Image className="footer-logo" src={Linkedin} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.medium.com/@slavi.svec" target="_blank">
+          <Image className="footer-logo" src={Medium} />
         </a>
       </li>
       <li>
         <a href="https://mobile.twitter.com/SlaviSvec" target="_blank">
-          <Image src={Twitter} />
+          <Image className="footer-logo" src={Twitter} />
         </a>
       </li>
       <li>
         <a href="https://github.com/Slavi-Svec" target="_blank">
-          <Image src={GitHub} />
+          <Image className="footer-logo" src={GitHub} />
         </a>
       </li>
       <li>
         <a href="https://codepen.io/slavi-svec" target="_blank">
-          <Image className="footer-logo" src={CodePen} />
+          <Image
+            className="footer-logo"
+            className="footer-logo"
+            src={CodePen}
+          />
         </a>
       </li>
     </ul>
