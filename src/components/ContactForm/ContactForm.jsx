5be18fa924d0c90709c355b586e61a75dc466875
izaak svec <slavi.svec@gmail.com>
2021-07-19 00:24:48 +1000

    Add styles to contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,25 +9,46 @@ const ContactForm = () => {
         action="https://formsubmit.co/slavi.svec@gmail.com"
         method="POST"
       >
+        <input type="hidden" name="_captcha" value="false" />
+        <input
+          type="hidden"
+          name="_autoresponse"
+          value="Thanks for getting in touch, I will get back to you ASAP"
+        />
         <input
           type="hidden"
           name="_next"
-          value="http://localhost:3002/contact"
+          value="https://www.slavisvec.com/contact"
         />
         <input
           type="hidden"
           name="_subject"
           value="Hi, Someone has contacted you"
         />
-        <input type="text" name="name" placeholder="Enter an name" required />
         <input
+          className="form-field"
+          type="text"
+          name="name"
+          placeholder="Enter an name"
+          required
+        />
+        <input
+          className="form-field"
           type="text"
           name="message"
           placeholder="write your message here"
           required
         />
-        <input type="email" name="email" placeholder="Email Address" required />
-        <button type="submit">Send</button>
+        <input
+          className="form-field"
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          required
+        />
+        <button className="form-button" type="submit">
+          Send
+        </button>
       </form>
     </div>
   )
